1d719017abbafd8f59eba9737e9e79f330c5b488
Sean Febo <seanfebo@techie.com>
2018-05-17 20:21:11 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,23 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
-
+  get '/posts/new' do
+    erb :new 
   end
+  
+  post '/posts' do 
+    @post = Post.create(params)
+    redirect '/posts'
+  end
+  
+  get '/posts' do 
+    @posts = Post.all
+    erb :index 
+  end
+  
+  get 'posts/:id' do 
+    @post = Post.find_by_id(params[:id])
+    erb :show 
+  end
+    
 end
